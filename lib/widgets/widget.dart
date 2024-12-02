e9c438d44c83495ba026c10ca9f707fc549e5d8c
dagnarys <super.iard@yandex.ru>
2024-12-02 22:02:11 +0300

    added stack with dark blackg

--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/widgets/SearchWidget.dart';
 import 'package:flutter_application_2/widgets/TextLocationWidget.dart';
 
 class Example extends StatelessWidget {
@@ -6,10 +7,51 @@ class Example extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Textlocationwidget()
+    return Stack(
+      children: [
+      Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color(0xFF313131),
+            Color(0xFF111111),
+          ],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+          ),
+        ),
+        height: 280,
       ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 68,),
+          Center(
+            child: Container(
+              width: 327,
+              height: 140,
+              child: const Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Textlocationwidget(),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    SearchWidget(),
+                  ],
+                )
+              ),
+            ),
+          ),
+          SizedBox(height: 24,),
+          Container(
+            //add here promo
+          )
+
+        ],
+      ),
+      ],
     );
   }
 }
