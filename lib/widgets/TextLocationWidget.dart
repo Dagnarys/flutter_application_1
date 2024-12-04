5ccd621fd87fa5dffd4ed5db0f2ad27580c09567
dagnarys <super.iard@yandex.ru>
2024-12-04 20:23:16 +0300

    Update TextLocationWidget.dart

--- lib/widgets/TextLocationWidget.dart
+++ lib/widgets/TextLocationWidget.dart
@@ -11,35 +11,35 @@ class Textlocationwidget extends StatelessWidget {
       child: const Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.center,
-        
         children: [
-          Text('Location',textAlign: TextAlign.end,
-           style: TextStyle(
-
-            color: Color.fromRGBO(230, 230, 230, 100),
-            fontSize: 14, 
-            fontWeight: FontWeight.w300
-          ),),      
-          DropdownLocation(),         
-        ],    
+          Text(
+            'Location',
+            textAlign: TextAlign.end,
+            style: TextStyle(
+                color: Color.fromRGBO(230, 230, 230, 100),
+                fontSize: 14,
+                fontWeight: FontWeight.w300),
+          ),
+          DropdownLocation(),
+        ],
       ),
     );
   }
 }
-// 
+//
 
 class Location {
   String city;
   String district;
 
-  Location({required this.city, required this.district });
+  Location({required this.city, required this.district});
   @override
   String toString() {
     return 'Town: $city, District,$district';
-
   }
 }
-List <Location> listLocation = [
+
+List<Location> listLocation = [
   Location(city: 'Blizen', district: 'Tanjungbalai'),
   Location(city: 'Jakarta', district: 'Jakarta Selatan'),
   Location(city: 'Surabaya', district: 'Kota Surabaya'),
@@ -52,32 +52,28 @@ class DropdownLocation extends StatefulWidget {
   @override
   State<DropdownLocation> createState() => _DropdownLocationState();
 }
+
 class _DropdownLocationState extends State<DropdownLocation> {
   Location dropDownLocation = listLocation.first;
 
   @override
   Widget build(BuildContext context) {
     return DropdownButton<Location>(
-  
       padding: const EdgeInsets.all(0),
-      
       icon: const Icon(Icons.keyboard_arrow_down),
-
       isExpanded: true,
-      
-      value : dropDownLocation,
+      value: dropDownLocation,
       items: listLocation.map<DropdownMenuItem<Location>>((Location location) {
-        return DropdownMenuItem <Location>(
+        return DropdownMenuItem<Location>(
           value: location,
-          child: Text('${location.city}, ${location.district}',
-          style: const TextStyle(
-             
-            
-            color: Color.fromRGBO(230, 230, 230, 100),
-            fontSize: 14, 
-            fontWeight: FontWeight.w600
-          ),), // Отображение имени
-          );
+          child: Text(
+            '${location.city}, ${location.district}',
+            style: const TextStyle(
+                color: Color.fromRGBO(230, 230, 230, 100),
+                fontSize: 14,
+                fontWeight: FontWeight.w600),
+          ), // Отображение имени
+        );
       }).toList(),
       dropdownColor: Color(0xFF313131),
       underline: Container(),
@@ -88,7 +84,6 @@ class _DropdownLocationState extends State<DropdownLocation> {
           dropDownLocation = newValue!;
         });
       },
-
     );
   }
 }
