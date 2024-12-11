99f56dd206e9e23ec0a4271096f5d75fdbec43e1
dagnarys <super.iard@yandex.ru>
2024-12-11 21:45:45 +0300

    Create CardListWidget.dart

---
+++ lib/widgets/CardListWidget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CardListwidget extends StatelessWidget {
+  const CardListwidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  ListView(
+
+    );
+  }
+}
+
+
+class CardWidget extends StatelessWidget {
+  const CardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      height: 238,
+      width: 156,
+      color: Colors.amber,
+    );
+  }
+}
